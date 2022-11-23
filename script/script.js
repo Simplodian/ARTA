7a68a48de40c144e8585141bfc62f0064b323832
Aliaksandr Zozon <sashazozon1337@gmail.com>
2022-11-23 10:57:02 +0300

    feat: add number form and number check

--- script/script.js
+++ script/script.js
@@ -10,3 +10,17 @@ function overlayActive(){
 
 openOverlayBtn.addEventListener('click', overlayActive);
 closeOverlayBtn.addEventListener('click', overlayActive);
+
+const telNumber = document.querySelector('.overlay__number'),
+      overlaySubmit = document.querySelector('.overlay__submit');
+
+
+overlaySubmit.onclick = function(){
+  let i = telNumber.value;
+  console.log(i.length)
+  if ( i.slice(0,4) != '+375' || i.length != 16 || i.slice(4,6) != '29' && i.slice(4,6) != '25' && i.slice(4,6) != '44'){
+    alert('Введите телефон формата +375(**) *** - ** - **')
+  } else {
+    alert( 'Ваша заявка обрабатывается!')
+  }
+};

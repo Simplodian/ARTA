3c6e8a7144077508d74fd7a59809ee8cc9cf1b2c
Aliaksandr Zozon <sashazozon1337@gmail.com>
2022-11-23 10:59:38 +0300

    fix: changed values of number test

--- script/script.js
+++ script/script.js
@@ -18,7 +18,7 @@ const telNumber = document.querySelector('.overlay__number'),
 overlaySubmit.onclick = function(){
   let i = telNumber.value;
   console.log(i.length)
-  if ( i.slice(0,4) != '+375' || i.length != 16 || i.slice(4,6) != '29' && i.slice(4,6) != '25' && i.slice(4,6) != '44'){
+  if ( i.slice(0,4) != '+375' || i.length != 13 || i.slice(4,6) != '29' && i.slice(4,6) != '25' && i.slice(4,6) != '44'){
     alert('Введите телефон формата +375(**) *** - ** - **')
   } else {
     alert( 'Ваша заявка обрабатывается!')

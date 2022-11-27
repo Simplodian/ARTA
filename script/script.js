3c3234901fde850f9899da84221e7c6beec1612e
Aliaksandr Zozon <sashazozon1337@gmail.com>
2022-11-28 01:35:59 +0300

    add remarks

--- script/script.js
+++ script/script.js
@@ -1,7 +1,8 @@
+'use strict';
 const openOverlayBtn = document.querySelector('.submit');
 const closeOverlayBtn = document.querySelector('.overlay__close-btn');
-const overlay = document.querySelector('.overlay')
-const callback = document.querySelector('.callback')
+const overlay = document.querySelector('.overlay');
+const callback = document.querySelector('.callback');
 
 function overlayActive() {
   overlay.classList.toggle('overlay-active');
@@ -12,22 +13,21 @@ openOverlayBtn.addEventListener('click', overlayActive);
 closeOverlayBtn.addEventListener('click', overlayActive);
 
 const telNumber = document.querySelector('.overlay__number'),
-  overlaySubmit = document.querySelector('.overlay__submit');
+      overlaySubmit = document.querySelector('.overlay__submit');
 
 
 overlaySubmit.onclick = function () {
   let i = telNumber.value;
-  console.log(i.length)
   if (i.slice(0, 4) != '+375' || i.length != 13 || i.slice(4, 6) != '29' && i.slice(4, 6) != '25' && i.slice(4, 6) != '44') {
-    alert('Введите телефон формата +375(**) *** - ** - **')
+    alert('Введите телефон формата +375(**) *** - ** - **');
   } else {
-    alert('Ваша заявка обрабатывается!')
+    alert('Ваша заявка обрабатывается!');
   }
 };
 
 
-const openGallary = document.querySelectorAll('.designation');
-const gallary = document.querySelectorAll('.designation-content');
+const openGallary = document.querySelectorAll('.designation'),
+      gallary = document.querySelectorAll('.designation-content');
 
 openGallary.forEach((item, index) => {
   item.addEventListener('click', () => {
@@ -44,9 +44,9 @@ openGallary.forEach((item, index) => {
     } else if(index == 5){
       gallary[5].classList.toggle('designation-content-active-6');
     }else{
-      console.log(undefined)
+      console.log(undefined);
     }
-  })
-})
+  });
+});
 
-console.log(gallary)
+console.log(gallary);
